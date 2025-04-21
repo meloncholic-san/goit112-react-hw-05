a52b1e74fc2ee9d0c2123d6524e45250b1aeb020
Panchenko Denis <Pandenisko@gmail.com>
2025-04-21 21:11:52 +0300

    Update MovieItem.jsx

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,17 +1,3 @@
-// import { Link, useLocation } from "react-router";
-
-// export default function MovieItem ({movie}) {
-
-//     const location = useLocation();
-//     return (
-//         <Link
-//         to={`/movies/${movie.id}`}
-//         state={{ previousLocation: location }}
-//         > 
-//         {movie.title}
-//         </Link>
-//     )
-// }
 
 import { Link, useLocation } from "react-router";
 
